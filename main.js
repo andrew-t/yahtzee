f0258e5850df391f8e25645d027339e4eb95dc0c
Andrew <andrew@truepill.com>
2020-04-17 14:36:23 +0100

    Prompt for number of players

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 import './components/dice/dice-roller.js';
 import './components/scoreboard/yahtzee-scoreboard.js';
 
+const numberOfPlayers = prompt('How many players?');
+
+document.getElementById('game').innerHTML += `
+	<yahtzee-scoreboard
+		id="scores"
+		players="${numberOfPlayers}">
+	</yahtzee-scoreboard>
+	<dice-roller
+		id="roller"
+		count="5"
+		downscaling="1">
+	</dice-roller>
+`;
+
 const dice = document.getElementById('roller'),
 	scoreboard = document.getElementById('scores');
+
+play();
 
 async function play() {
 	while (!scoreboard.gameOver) {
@@ -10,5 +26,3 @@ async function play() {
 		await scoreboard.scoreDice(roll);
 	}
 }
-
-play();
